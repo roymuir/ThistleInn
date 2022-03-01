aa449c9de88a4aa7ad4e3e288064a569b6a664fd
Roy Muir <roym@roymuir.com>
2022-03-01 20:40:51 +1300

    Remove old GA code from html

--- sites/default/themes/thistleinn/templates/html.tpl.php
+++ sites/default/themes/thistleinn/templates/html.tpl.php
@@ -93,16 +93,6 @@ $html_attributes = "lang=\"{$language->language}\" dir=\"{$language->dir}\" {$rd
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-64407624-1', 'auto');
-    ga('send', 'pageview');
-  </script>
  
 </body>
 </html>
